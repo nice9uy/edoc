5c3b76562ede1a966f27be463508848d59be906a
nice9uy <abdulrachman.mails@gmail.com>
2023-11-06 14:17:41 +0700

    add kontrak db

--- kontrak_app/models.py
+++ kontrak_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+def user_folder(instance, filename):
+    return f"{instance.username}/kontrak/{filename}"
+
+class Kontrak(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    id_user = models.CharField(max_length=4)
+    username = models.CharField(max_length=30)
+    no_kontrak = models.IntegerField()
+    sumber_dana = models.CharField(max_length=6)
+    tgl = models.DateField()
+    tentang = models.CharField(max_length=200)
+    upload_file = models.FileField(upload_to=user_folder, null=False, blank=False)
+    today = models.DateField()
+    tahun = models.CharField(max_length=4)
--- kontrak_app/views.py
+++ kontrak_app/views.py
@@ -6,12 +6,12 @@ from django.shortcuts import render
 
 def kontrak(request):
 
-    # id_username = request.user.pk
+    id_username = request.user.pk
 
-    # user = request.user
+    user = request.user
 
     # klasifikasi = DatabaseSurat.objects.filter(id_user = id_username).values_list("nama_klasifikasi" , flat=True)
     # kelompok = KelompokSurat.objects.filter(id_user = id_username).values_list("nama_kelompok", flat=True)
    
 
-    return render (request, 'kontrak/pages/index.html')
+    return render (request, 'kontrak/pages/index.html') 
